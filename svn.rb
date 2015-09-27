81bc3f94cb71be69b9f18ce53a1f478e474a019e
Aboobacker MK <aboobackervyd@gmail.com>
2015-09-27 13:54:49 +0530

    added support for path

--- svn.rb
+++ svn.rb
@@ -4,9 +4,9 @@ require 'uri'
 module SVN
 
   class << self
-    attr_accessor :username, :password
+    attr_accessor :username, :password,:path
   end
-  
+
   # Returns a string to be passed into commands containing authentication options
   # Requires setting of username and password via attr_accessor methods
   def self.authentication_details
@@ -20,24 +20,24 @@ module SVN
       [$1, $2]
     end
   end
-  
+
   # Adds the given path to the working copy
   def self.add(path)
     SVN.execute("add #{path}")
   end
-  
+
   # Adds all new or modified files to the working copy
   def self.add_all
     SVN.status.each { |file| add = SVN.add(file[1]) if file[0] == '?' }
   end
-  
+
   # Add all new or modified files to the working copy and commits changes
   # An optional commit message can be passed if required
   def self.add_and_commit_all(message=nil)
     SVN.add_all
     SVN.commit message
   end
-  
+
   # Commits all changes, and returns the new revision number
   # An optional commit message can be passed if required
   def self.commit(message=nil)
@@ -52,7 +52,7 @@ module SVN
       return nil
     end
   end
-  
+
   # Returns a diff of two commits based on their respective revision numbers
   # (first and second arguments) and a repository path (third argument)
   def self.diff(revision_1,revision_2,file=nil)
@@ -81,7 +81,11 @@ module SVN
   private
 
   def self.execute(command)
-    %x{svn #{command} #{SVN.authentication_details}}
+    if @path.nil?
+      return %x{svn #{command} #{SVN.authentication_details}}
+    else
+      return %x{svn #{command} #{SVN.authentication_details} #{@path}}
+    end
   end
 
 end
