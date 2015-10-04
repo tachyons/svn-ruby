10ff7158a55d55096dd8981620ac8ae2c90b924b
Aboobacker MK <aboobackervyd@gmail.com>
2015-10-04 21:44:22 +0530

    xml to Revision

--- lib/revision.rb
+++ lib/revision.rb
@@ -1,13 +1,19 @@
 module SVN
   class Revision
     attr_accessor :author,:date,:message,:revision_no
-    def self.make_from_xml_doc
+    def self.make_from_xml_doc(doc)
+      revisions=[]
       doc.css("logentry").each do |logentry|
-        revision_no=logentry.attributes["revision"].content
-        author= logentry.at_css("author").content
-        date= logentry.at_css("date").content
-        message= logentry.at_css("msg").content
+        revision=SVN::Revision.new
+        revision.revision_no=logentry.attributes["revision"].content
+        revision.author= logentry.at_css("author").content
+        revision.date= logentry.at_css("date").content
+        revision.message= logentry.at_css("msg").content
+        revisions <<revision
       end
+      return revisions
+    end
+    def list
     end
   end
 end
--- sample.rb
+++ sample.rb
@@ -8,9 +8,4 @@ require_relative 'lib/svn.rb'
 f = File.open("sample.xml")
 doc = Nokogiri::XML(f)
 f.close
-doc.css("logentry").each do |logentry|
-  revision=logentry.attributes["revision"].content
-  author= logentry.at_css("author").content
-  date= logentry.at_css("date").content
-  msg= logentry.at_css("msg").content
-end
+revisions=SVN::Revision.make_from_xml_doc(doc)
